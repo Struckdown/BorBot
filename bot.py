fa7f44d0abfd6bd16507c8b5d0c9f08886378efb
Struckdown <bfleysher@gmail.com>
2020-09-25 21:51:02 -0600

    Initial commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_SERVER')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        print(f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})')
+
+        if guild.name == GUILD:
+            break
+
+client.run(TOKEN)
+
