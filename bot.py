3ffcc3de00c8636cd12b164569f85ed111a3b164
Struckdown <bfleysher@gmail.com>
2020-09-29 18:44:53 -0600

    Added 8ball command

--- bot.py
+++ bot.py
@@ -190,4 +190,41 @@ async def hexagon(ctx, text:str):
 	await ctx.channel.send(file=discord.File("hexagon.png"))
 
 
+@bot.command(name="ask", help="Recieve an answer from the bot about whatever questions you might have")
+async def magicball(ctx, *, text:str):
+
+	randomReplies = [
+	"It is certain.",
+	"It is decidedly so.",
+	"Without a doubt.",
+	"Yes – definitely.",
+	"You may rely on it.",
+	"As I see it, yes.",
+	"Most likely.",
+	"Outlook good.",
+	"Yes.",
+	"Signs point to yes.",
+	"Reply hazy, try again.",
+	"Ask again later.",
+	"Better not tell you now.",
+	"Cannot predict now.",
+	"Concentrate and ask again.",
+	"Don't count on it.",
+	"My reply is no.",
+	"My sources say no.",
+	"Outlook not so good.",
+	"Very doubtful.",
+	]
+
+	i = random.randint(0, len(randomReplies)-1)
+	randomReply = randomReplies[i]
+
+	await ctx.channel.send(randomReply)
+
+
+@bot.command(name="echo", help="Have the bot say something!")
+async def echo(ctx, channel, *, text:str):
+	pass
+
+
 bot.run(TOKEN)
